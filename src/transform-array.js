a1809074726f68b3e66124b0989471e8ecadf055
edkondr <edyard2002@mail.ru>
2020-11-01 22:43:25 +0300

    fix:file

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,15 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let changeArr = [];
+    if (arr.length === 0) return arr;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === '--discard-next'){
+            i++;
+        } else if(arr[i] === '--discard-prev'){
+            changeArr.pop() /*удаляем последний элемент массива и возвращаем его*/
+        } else if(arr[i] === '--double-next'){
+            changeArr.push(arr[i + 1]); /*добавляем элементы в конец массива*/
+        } else if(arr[i] === '--double-prev'){
+            changeArr.push(arr[i - 1])
+        }else  changeArr.push(arr[i])
+    }return changeArr;
 };
