8079e848d21b98b16899b36ce686a41b335360e6
edkondr <edyard2002@mail.ru>
2020-11-01 20:43:24 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
     
     let count = (Math.pow(2, disksNumber) - 1);
-    return {turns: count,seconds: count / (turnsSpeed / 3600),};
+    return {turns: count,seconds: (Math.floor(count / (turnsSpeed / 3600)))};
 }
